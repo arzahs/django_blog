53ad71ef00036bd679f69b7a954286e4e41178f2
Sergey Nelepa <nelepa1995@mail.ru>
2015-09-06 19:37:05 +0300

    work with static, added foundation

--- firstapp/settings.py
+++ firstapp/settings.py
@@ -105,3 +105,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    ('static', '/home/sergey/djangoenv/bin/firstapp/static/'),
+    #os.path.join(BASE_DIR, "static"),
+    #'/var/www/static/',
+)
