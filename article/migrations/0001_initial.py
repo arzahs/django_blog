68eac851efb989dfc33c903cd7ec9049b18cd579
Sergey Nelepa <nelepa1995@mail.ru>
2015-09-09 15:31:01 +0300

    pagination for page

---
+++ article/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
+                ('article_title', models.CharField(max_length=200)),
+                ('article_text', models.TextField()),
+                ('article_date', models.DateTimeField()),
+                ('article_likes', models.IntegerField()),
+            ],
+            options={
+                'db_table': 'article',
+            },
+        ),
+    ]
